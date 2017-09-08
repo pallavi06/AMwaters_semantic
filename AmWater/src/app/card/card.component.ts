e5bec3078e6be653201ee601751cffedfe62d206
pallavi06 <pallavi.more@accionlabs.com>
2017-09-08 13:06:43 +0530

    card layout css updated

--- AmWater/src/app/card/card.component.ts
+++ AmWater/src/app/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CardComponent implements OnInit {
   @Input() heading;
