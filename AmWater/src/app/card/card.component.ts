9e79a3191fb2f87a7adcd963dbcca937a5cc6f7a
95deeptikaskar <[deepti.kaskar@accionlabs.com]>
2017-09-08 11:33:59 -0700

    reduced redundancy of card layout

--- AmWater/src/app/card/card.component.ts
+++ AmWater/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  @Input() heading;
+  @Input() text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo sed do eiusmod tempo';
+  @Input() img_src ;
   constructor() { }
 
   ngOnInit() {
